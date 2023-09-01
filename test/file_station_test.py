ef11a0e26fcda9821346e7a32ee2ebc7ee7a23ca
wangxso <wangxs0102@gmail.com>
2023-09-01 16:26:44 +0800

    add logger module

--- test/file_station_test.py
+++ test/file_station_test.py
@@ -2,6 +2,7 @@ import unittest
 from api import file_station, system
 from main import session
 from config import USRNAME, PASSWORD
+from loguru import logger
 class FileStationTest(unittest.TestCase):
     def setUp(self) -> None:
         system.DSMLogin(USRNAME, PASSWORD)
@@ -14,25 +15,31 @@ class FileStationTest(unittest.TestCase):
     def test_FileInfo(self):
         
         res = file_station.FileInfo()
+        logger.info(res)
         self.assertTrue(res['success'])
     
     def test_FileDownload(self):
         res = file_station.FileDownload("/Download/test.jpg")
+        logger.info(res)
         self.assertTrue(res['success'])
 
     def test_CreateFolder(self):
         res = file_station.CreateFolder('/Download', 'test_1')
+        logger.info(res)
         self.assertTrue(res['success'])
     def test_RenameFileorFolder(self):
         res = file_station.RenameFileorFolder('/Download/test_1', 'test_2')
+        logger.info(res)
         self.assertTrue(res['success'])
     
     def test_BlockingFileDelete(self):
         res = file_station.DeleteFolderOrFiles('/Download/test_2')
+        logger.info(res)
         self.assertTrue(res['success'])
     
     def test_FolderList(self):
         res = file_station.FolderList()
+        logger.info(res)
         self.assertTrue(res['success'])
     
 if __name__ == "__main__":
--- test/system_test.py
+++ test/system_test.py
@@ -1,11 +1,20 @@
 import unittest
 from api import system
+from config import USRNAME, PASSWORD
+from loguru import logger
+
 class SystemTest(unittest.TestCase):
 
     def test_LoginErrorUsername(self):
         resp = system.DSMLogin('xxx', 'xxx')
+        logger.info(resp)
         self.assertFalse(resp['success'])
     
+    def test_DSMInfo(self):
+        system.DSMLogin(USRNAME, PASSWORD)
+        res = system.DSMInfo()
+        logger.info(res)
+        self.assertTrue(res['success'])
 
 if __name__ == '__main__':
     unittest.main()
